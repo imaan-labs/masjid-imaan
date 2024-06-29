536c9eb40dcc5928602f1edf6f121a1239b5dd41
Musab Jafri <musabmjafri@gmail.com>
2024-06-28 20:08:11 -0500

    Form update

--- app/registrations/student-programs/page.tsx
+++ app/registrations/student-programs/page.tsx
@@ -8,7 +8,7 @@ export default async function StudentPrograms() {
         <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
             <div className="mx-auto max-w-screen-sm text-center">
                 <Iframe
-                url="https://f7e6686a.sibforms.com/serve/MUIFAGjQZL5Udj1zBcE6wiw23LX1DT6t-2AqftK8OU6G6xM3ba_Y87nZsYsKa6Exc_mG8kP3KEMwDBQj5zoy1yVmYaY1UnbFIvXRnmFpzOvrBKTLtM_leGmT1cFgVMHjpG1_yyvj_9oOfGWgVY1nn-Sqp1XBx7o0-oTGWsA8AQO3Mx4tgf4OqSLIqhE4FXCC88c1PXDt56sHNReU"
+                url="https://f7e6686a.sibforms.com/serve/MUIFAMcSDj7z0R8SiDqo_guGUdxfDddLVRLSjlWnzJj2XvJDA0FtFR1JQ81EttF2Bv10jUNIGWpiQz26YHUofFspk_YA29aFpxuWNquzd1H6XMDu8fWMhC1_XQanfcxyr0eoiYiVEGw5TJcseLD3HxXb4_LCLTk68L8TMSrmJVyirPQfa6LQPQAFKUPYOmZj9EzgFW6CYx8vLuNq"
                 id="brevo"
                 className="min-h-screen w-full"
                 display="block"
