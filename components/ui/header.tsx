7602259f8a5cd4ec345e711fbc5531f0cd15131d
zishack786 <zishack786@gmail.com>
2024-09-12 01:37:34 -0500

    add wishlist

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -78,6 +78,14 @@ export default function Header() {
                         Gallery
                       </Link>
                     </li>
+                    <li>
+                      <Link
+                        href="/wishlist"
+                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      >
+                        Wishlist
+                      </Link>
+                    </li>
                   </ul>
                 </div>
               </li>
