996b4ee64270b956834ae951e649f7680c962041
Musab Jafri <musabmjafri@gmail.com>
2025-03-24 11:38:11 -0500

    zigzag taraweeh addition

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 import FeatImage01 from "@/public/images/jummah.jpg";
 import FeatImage02 from "@/public/images/firstsaturdaylecture.jpeg";
-import FeatImage03 from "@/public/images/features-03-image-03.png";
+import FeatImage03 from "@/public/images/taraweeh.jpg";
 
 export default function Zigzag() {
   return (
@@ -90,7 +90,7 @@ export default function Zigzag() {
                         English Lecture
                       </span>
                     </li>
-                    <li className="flex items-center">
+                    <li className="flex items-center mb-2">
                       <svg
                         className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
                         viewBox="0 0 12 12"
@@ -102,7 +102,7 @@ export default function Zigzag() {
                         Second Adhan
                       </span>
                     </li>
-                    <li className="flex items-center">
+                    <li className="flex items-center mb-2">
                       <svg
                         className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
                         viewBox="0 0 12 12"
@@ -170,16 +170,6 @@ export default function Zigzag() {
                       >
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Ask your questions in English or Urdu!</span>
-                    </li>
-                    <li className="flex items-center mb-2">
-                      <svg
-                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
-                        viewBox="0 0 12 12"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
-                      </svg>
                       <span>Sisters have their own mic to ask questions!</span>
                     </li>
                     <li className="flex items-center">
@@ -191,6 +181,62 @@ export default function Zigzag() {
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
                       <span>All questions are welcome!</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            {/* 3rd item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl"
+                data-aos="fade-up"
+              >
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src={FeatImage03}
+                  width={540}
+                  height={405}
+                  alt="Features 03"
+                />
+              </div>
+              {/* Content */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                data-aos="fade-right"
+              >
+                <div className="md:pl-4 lg:pl-12 xl:pl-16">
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">
+                    Join Taraweeh every Ramadan!
+                  </div>
+                  <h3 className="h3 mb-3">
+                    Complete the Qur'an behind a Hafidh!
+                  </h3>
+                  <p className="text-xl text-gray-400 mb-4">
+                    And learn about what each Juz in the Qur'an talks about with an English lecture supplimenting the Taraweeh by an Aa'lim.
+                  </p>
+                  <ul className="text-lg text-gray-400 -mb-2">
+                    <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Sister have access to view the Imam directly for Salah and Lectures</span>
+                    </li>
+                    <li className="flex items-center">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Children have a playroom</span>
                     </li>
                   </ul>
                 </div>
