b7e9fd75804ea280f67eac229cd8a126497bd7f8
Musab Jafri <musabmjafri@gmail.com>
2025-03-28 13:31:33 -0500

    Zigzag fix

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -191,7 +191,7 @@ export default function Zigzag() {
             <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
               {/* Image */}
               <div
-                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0"
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1"
                 data-aos="fade-up"
               >
                 <Image
