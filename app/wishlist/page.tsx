9002e387970f29668dc7857986280d860f66e152
Musab Jafri <musabmjafri@gmail.com>
2025-04-01 13:32:33 -0500

    changing Link object to anchor

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -114,12 +114,12 @@ export default function WishlistPage() {
         </div>
       </section>
       <div className="mx-auto max-w-screen-sm text-center">
-        <Link
+        <a
           href="/"
           className="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4"
           >
           Back to Homepage
-        </Link>
+        </a>
       </div>
       <Footer />
     </div>
