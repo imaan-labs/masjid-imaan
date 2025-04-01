fe888acf0a2f50ab397e7e32c7975dcea713dd9b
Musab Jafri <musabmjafri@gmail.com>
2025-03-31 23:52:10 -0500

    Resources renamed to "More..."

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -52,7 +52,7 @@ export default function Header() {
                   className="text-black hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                   type="button"
                 >
-                  Resources
+                  More...
                   <svg
                     className="w-2.5 h-2.5 ms-3"
                     aria-hidden="true"
