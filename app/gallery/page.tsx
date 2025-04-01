8f03dec69b53c792306c002409fdc271df1ab37e
Musab Jafri <musabmjafri@gmail.com>
2025-04-01 13:00:54 -0500

    deleting forms, added home button to gallery & wishlist

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -93,6 +93,14 @@ export default function GalleryPage() {
             </div>
           ))}
       </div>
+      <div className="mx-auto max-w-screen-sm text-center">
+        <Link
+          href="/"
+          className="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4"
+          >
+          Back to Homepage
+        </Link>
+      </div>
     </div>
   );
 }
