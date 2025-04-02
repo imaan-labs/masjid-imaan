04cdd4332730ae49e198491dd343d407c25d14dc
Musab Jafri <musabmjafri@gmail.com>
2025-04-01 21:01:58 -0500

    back to home button brought up

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -95,6 +95,14 @@ export default function GalleryPage() {
 
   return (
     <div className="pt-20">
+    <div className="mx-auto max-w-screen-sm text-center">
+      <a
+        href="/"
+        className="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4"
+        >
+        Back to Homepage
+      </a>
+    </div>
       <div className="flex items-center justify-center py-4 md:py-8 flex-wrap">
         {categories.map((category, index) => (
           <button
@@ -127,14 +135,6 @@ export default function GalleryPage() {
               />
             </div>
           ))}
-      </div>
-      <div className="mx-auto max-w-screen-sm text-center">
-        <a
-          href="/"
-          className="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4"
-          >
-          Back to Homepage
-        </a>
       </div>
     </div>
   );
