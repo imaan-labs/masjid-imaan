4ff851d4a9776825bdaa97e7cc454ee87fcab041
Musab Jafri <musabmjafri@gmail.com>
2026-04-01 14:45:03 -0500

    Tafsir information updated

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -157,7 +157,7 @@ export default function Zigzag() {
               >
                 <div className="md:pl-4 lg:pl-12 xl:pl-16">
                   <div className="font-architects-daughter text-xl text-purple-600 mb-2">
-                    Seem Ilm! Seek Knowledge!
+                    Listen to Tafir! Seek Knowledge!
                   </div>
                   <h3 className="h3 mb-3">
                     Have Questions? Come Ask An Aa'lim
@@ -174,7 +174,27 @@ export default function Zigzag() {
                       >
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Sisters have their own mic to ask questions!</span>
+                      <span>Interactive sessions</span>
+                    </li>
+                    <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Every weekend</span>
+                    </li>
+                    <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Sisters have their own mic to ask questions</span>
                     </li>
                     <li className="flex items-center">
                       <svg
@@ -184,7 +204,7 @@ export default function Zigzag() {
                       >
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>All questions are welcome!</span>
+                      <span>All questions are welcome</span>
                     </li>
                   </ul>
                 </div>
